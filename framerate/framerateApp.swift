f14986f77f84a231faa02cab1b32fd8ed34df5da
liupeng <liupeng3202012@gmail.com>
2022-02-03 13:03:17 +0800

    Initial Commit

---
+++ framerate/framerateApp.swift
@@ -0,0 +1,17 @@
+//
+//  framerateApp.swift
+//  framerate
+//
+//  Created by 刘鹏 on 2022/2/3.
+//
+
+import SwiftUI
+
+@main
+struct framerateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
